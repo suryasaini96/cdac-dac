63ba37b11638d37697ac25c3db1ad3df05dd41d5
suryasaini96 <champ121991@gmail.com>
2021-06-10 14:15:48 +0530

    Added Lab exam backups for COP and OS

---
+++ COP/COP_Lab_Exam_Backup/src/Pattern.java
@@ -0,0 +1,16 @@
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		for (int i=7; i>=1; i--) {
+			for (int j=i; j>=1; j--) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		
+		/* Simple pattern just traverse in reverse */
+		
+	}
+
+}
