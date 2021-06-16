fad1efc84fff05600f123943c2825cae59bb83dc
suryasaini96 <champ121991@gmail.com>
2021-06-16 22:27:11 +0530

    Minor changes in assignment 5

--- OOP with Java/Assignment 5/Emp/src/com/app/org/Emp.java
+++ OOP with Java/Assignment 5/Emp/src/com/app/org/Emp.java
@@ -1,25 +1,23 @@
 package com.app.org;
 
-public class Emp{
+public abstract class Emp{
     protected int id;
     protected String name;
     protected String deptId;
     protected double basic;
     
-    public Emp(int id, String name, String deptId, double basic) {
+    protected Emp(int id, String name, String deptId, double basic) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.deptId = deptId;
 		this.basic = basic;
 	}
+    
+    @Override
+	public String toString() {
+		return "Emp [id=" + id + ", name=" + name + ", deptId=" + deptId + ", basic=" + basic + "]";
+	}
 
-	@Override
-    public String toString(){
-        return "Emp [id=" + id + ", name=" + name + ", deptId=" + deptId + ", basic=" + basic + "]";
-    }
-
-    public double computeNetSalary(){
-        return basic;
-    }
+	protected abstract double computeNetSalary();
 }
