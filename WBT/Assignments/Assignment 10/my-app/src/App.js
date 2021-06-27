b07a3ab833f8990dae261d6364b75204ee054884
suryasaini96 <champ121991@gmail.com>
2021-06-27 23:29:26 +0530

    Minor display change

--- WBT/Assignments/Assignment 10/my-app/src/App.js
+++ WBT/Assignments/Assignment 10/my-app/src/App.js
@@ -4,24 +4,25 @@ import EmpDataComponent from './EmpDataComponent/EmpDataComponent';
 import ParentComponent from './SkillsListComponent/ParentComponent.js'
 import ToDoComponent from './ToDoComponent/ToDoComponent.js'
 
-// function App() {
-//   return (
-//     <div className="App">
-//         <EmpDataComponent />
-//         <ParentComponent />
-//     </div>
-//   );
-// }
-
-class App extends Component{
-  
-  render(){
-    return(
-      <div>
-          <ToDoComponent/>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+        <EmpDataComponent />
+        <ParentComponent />
+        <ToDoComponent/>
+    </div>
+  );
 }
+
+// class App extends Component{
+  
+//   render(){
+//     return(
+//       <div>
+//           <ToDoComponent/>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
