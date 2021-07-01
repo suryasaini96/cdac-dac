319935f5a7af49ca82ff8fbad55bf4540d5f02b9
suryasaini96 <champ121991@gmail.com>
2021-07-01 13:51:42 +0530

    DBT Assignment 1

---
+++ DBT/Assignment 1/Assignment 1.sql
@@ -0,0 +1,77 @@
+#1
+# mysql.exe -u root -p
+
+#2
+show databases;
+
+#3
+use information_schema;
+
+#4
+show tables;
+
+#5
+use mysql;
+
+#6
+# source D:\Libraries\Desktop\CDAC\Subjects\DBT\hrschema.sql
+use humanresource;
+
+#7
+show tables;
+
+#8
+desc countries;
+desc departments;
+desc emp_details_view;
+desc employees;
+desc job_history;
+desc jobs;
+desc locations;
+desc regions;
+
+#9
+select * from countries;
+select * from departments;
+select * from emp_details_view;
+select * from employees;
+select * from job_history;
+select * from jobs;
+select * from locations;
+select * from regions;
+
+#10
+#Interacted with MySQL Workbench
+
+#11
+# System cls;
+
+#12
+select current_user();
+
+#13
+select current_date(); # Date
+select current_time(); # Time
+select current_timestamp(); # Date with timestamp
+
+#14
+# Shows a null field with null column
+# Null means no data
+select null;
+
+#15
+create table mytable(
+	id int primary key,
+    name varchar(30),
+    email varchar(20),
+    dob date
+);
+
+
+
+
+
+
+
+
+
