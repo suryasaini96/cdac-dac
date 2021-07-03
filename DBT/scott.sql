c8cefdba81a004449eb73a4b572c4fc56615e1f6
suryasaini96 <champ121991@gmail.com>
2021-07-03 12:11:36 +0530

    Scott schema for MySQL

---
+++ DBT/scott.sql
@@ -0,0 +1,158 @@
+-- MySQL equivalent of Oracle SCOTT default schema.
+-- (datatypes are different but should generally be equivalent)
+--
+-- Oracle database installations reportedly have included a default
+-- schema called SCOTT, accessed by user "scott" with password "tiger".
+-- Wikipedia reports the SCOTT schema originated with Bruce Scott, one
+-- of the first employees at Oracle (then Software Development
+-- Laboratories), who had a cat named Tiger.
+--
+-- (1) Wikipedia contributors, "Oracle Database," Wikipedia, The Free
+-- Encyclopedia, http://en.wikipedia.org/w/index.php?title=Oracle_Database&oldid=416894489
+-- (accessed 2013/01/08).
+--
+
+-- phpMyAdmin SQL Dump
+-- version 3.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 08, 2013 at 11:02 PM
+-- Server version: 5.5.25a
+-- PHP Version: 5.4.4
+
+DROP SCHEMA
+
+IF EXISTS scott;
+	CREATE SCHEMA scott COLLATE = utf8_general_ci;
+
+USE scott;
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `scott`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bonus`
+--
+
+CREATE TABLE IF NOT EXISTS `bonus` (
+  `ENAME` varchar(10) DEFAULT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `SAL` int(11) DEFAULT NULL,
+  `COMM` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dept`
+--
+
+CREATE TABLE IF NOT EXISTS `dept` (
+  `DEPTNO` int(11) DEFAULT NULL,
+  `DNAME` varchar(14) DEFAULT NULL,
+  `LOC` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dept`
+--
+
+INSERT INTO `dept` (`DEPTNO`, `DNAME`, `LOC`) VALUES
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dummy`
+--
+
+CREATE TABLE IF NOT EXISTS `dummy` (
+  `DUMMY` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dummy`
+--
+
+INSERT INTO `dummy` (`DUMMY`) VALUES
+(0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp`
+--
+
+CREATE TABLE IF NOT EXISTS `emp` (
+  `EMPNO` int(11) NOT NULL,
+  `ENAME` varchar(10) DEFAULT NULL,
+  `JOB` varchar(9) DEFAULT NULL,
+  `MGR` int(11) DEFAULT NULL,
+  `HIREDATE` date DEFAULT NULL,
+  `SAL` int(11) DEFAULT NULL,
+  `COMM` int(11) DEFAULT NULL,
+  `DEPTNO` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `emp`
+--
+
+INSERT INTO `emp` (`EMPNO`, `ENAME`, `JOB`, `MGR`, `HIREDATE`, `SAL`, `COMM`, `DEPTNO`) VALUES
+(7369, 'SMITH', 'CLERK', 7902, '1980-12-17', 800, NULL, 20),
+(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600, 300, 30),
+(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250, 500, 30),
+(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975, NULL, 20),
+(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30),
+(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850, NULL, 30),
+(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450, NULL, 10),
+(7788, 'SCOTT', 'ANALYST', 7566, '1982-12-09', 3000, NULL, 20),
+(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000, NULL, 10),
+(7844, 'TURNER', 'SALESMAN', 7698, '1980-09-08', 1500, 0, 30),
+(7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', 1100, NULL, 20),
+(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950, NULL, 30),
+(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000, NULL, 20),
+(7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300, NULL, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salgrade`
+--
+
+CREATE TABLE IF NOT EXISTS `salgrade` (
+  `GRADE` int(11) DEFAULT NULL,
+  `LOSAL` int(11) DEFAULT NULL,
+  `HISAL` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `salgrade`
+--
+
+INSERT INTO `salgrade` (`GRADE`, `LOSAL`, `HISAL`) VALUES
+(1, 700, 1200),
+(2, 1201, 1400),
+(3, 1401, 2000),
+(4, 2001, 3000),
+(5, 3001, 9999);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
