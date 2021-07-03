dd27ee9fd8070483d5fd0434a6e039a20f50b6dd
suryasaini96 <champ121991@gmail.com>
2021-07-03 12:38:54 +0530

    scott.sql

--- DBT/scott.sql
+++ DBT/scott.sql
@@ -12,15 +12,6 @@
 -- (accessed 2013/01/08).
 --
 
--- phpMyAdmin SQL Dump
--- version 3.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 08, 2013 at 11:02 PM
--- Server version: 5.5.25a
--- PHP Version: 5.4.4
-
 DROP SCHEMA
 
 IF EXISTS scott;
@@ -31,21 +22,9 @@ USE scott;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
 -- Database: `scott`
 --
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `bonus`
---
 
 CREATE TABLE IF NOT EXISTS `bonus` (
   `ENAME` varchar(10) DEFAULT NULL,
@@ -152,7 +131,3 @@ INSERT INTO `salgrade` (`GRADE`, `LOSAL`, `HISAL`) VALUES
 (3, 1401, 2000),
 (4, 2001, 3000),
 (5, 3001, 9999);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
